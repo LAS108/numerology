10ca6b6a45d5aec0299a8626d0f45414fb76dab5
LAS108 <67728256+LAS108@users.noreply.github.com>
2020-08-28 10:33:05 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$('.block__title, .block__title2').click(function(event) {
+		if($('.block').hasClass('one')){
+			$('.block__title, .block__title2').not($(this)).removeClass('active');
+			$('.block__text').not($(this).next()).slideUp(1000);
+		}
+		$(this).toggleClass('active').next().slideToggle(1000);
+	});
+});
+
+$(document).ready(function() {
+	$('.slider').slick({
+	arrows:true,
+	adaptiveHeight:true,
+	slidesToShow:3,
+	initialSlide:0,
+	centerMode:false,
+	responsive: [
+   {
+      breakpoint: 1180,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+	});
+	
+});
